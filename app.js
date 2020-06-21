2921fa883cb63befe7855ec395f87b7db6913975
Dean <rladudwn0215@gmail.com>
2020-06-21 21:03:35 +0900

    app.js수정
    색 채워넣기 기능 추가

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITAL_COLOR;
+ctx.fillStyle = INITAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
   painting = false;
@@ -33,6 +39,28 @@ function onMouseMove(event) {
 function handleColorClick(event){
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+  if(filling === true){
+      filling = false;
+      mode.innerText = 'Fill';
+  } else {
+      filling = true;
+      mode.innerText = 'Paint';
+  }
+}
+
+function handleCanvasClick(event){
+  if(filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 }
 
 if(canvas) {
@@ -40,13 +68,21 @@ if(canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener('click', handleCanvasClick)
 }
 
+if(colors){
+
+}
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
 
+if(range){
+  range.addEventListener('input', handleRangeChange);
+}
 
-
-
+if(mode){
+  mode.addEventListener('click', handleModeClick);
+}
 
 
 
